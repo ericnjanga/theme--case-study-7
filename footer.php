a0ba7fac32abdd9f85fe33b1ef4c80eeece7635e
Eric Njanga <eric.njanga@gmail.com>
2023-08-24 01:20:17 -0400

    Footer development stage 1

--- footer.php
+++ footer.php
@@ -4,7 +4,16 @@
             
             <footer id="footer" role="contentinfo">
                 <div class="container bx-container">
-                    <div class="main-footer">
+
+                    <div class="block1">
+                        <img src="<?php echo get_template_directory_uri() .'/images/Stilettos & Hammers Logo.png' ?>" 
+                            alt="<?php echo esc_html( get_bloginfo( 'name' ) ) ?>" 
+                            class="logo"
+                        >
+                    </div>
+
+
+                    <div class="block2">
                         <div class="pages" role="contentinfo">
                             <h3>Pages</h3>
                             <?php
@@ -16,45 +25,44 @@
                             ?>
                         </div>
 
-                        <div class="services" role="contentinfo">
-                            <h3>Services</h3>
-                            <?php displayChildrenPageTitle('', 'services'); ?>
-                        </div>
-
-                        <div class="articles" role="contentinfo">
-                            <h3>Articles</h3>
-                            <?php latestPostTitles('', '', 3); ?> 
-                        </div>
-
-                        <div class="contact" role="contentinfo">
-                            <h3>Contact</h3>
-                            <?php dynamic_sidebar('sidebar-quick-contact'); ?>
-                        </div>
-
-                        <hr class="sep-1">
-
                         <div class="social-media" role="contentinfo">
                             <h3>Social Media</h3>
                             <?php dynamic_sidebar('sidebar-social-media'); ?>
                         </div>
 
+                        <div class="articles" role="contentinfo">
+                            <h3>Events</h3>
+                            <?php latestPostTitles('', '', 3); ?> 
+                        </div>
+
+                        <div class="articles" role="contentinfo">
+                            <h3>Vlog</h3>
+                            <?php latestPostTitles('', '', 3); ?> 
+                        </div>
+
+                        <div class="articles" role="contentinfo">
+                            <h3>Membership</h3>
+                        </div>
+                    </div>
+
+
+                    <div class="block3 text-center" role="contentinfo">
+                        <a href="#">Terms and Conditions</a> | <a href="#">Privacy Policy</a> | <span>&copy; <?php echo esc_html( date_i18n( __( 'Y', 'generic' ) ) ); ?> <?php echo esc_html( get_bloginfo( 'name' ) ); ?></span>
+                    </div>
+                    
+                
+                
+                
+                    <div class="main-footer">
+
+                        <!-- <hr class="sep-1">
+
                         <div class="appointment" role="contentinfo">
                             <h3>Appointment Scheduling</h3>
-                            <?php dynamic_sidebar('sidebar-appointments'); ?>
+                            <?php //dynamic_sidebar('sidebar-appointments'); ?>
                         </div>
 
-                        <hr class="sep-2">
-
-                        <div id="copyright" role="contentinfo">
-                            &copy; <?php echo esc_html( date_i18n( __( 'Y', 'generic' ) ) ); ?> <?php echo esc_html( get_bloginfo( 'name' ) ); ?>
-                        </div>
-                        
-                        <img 
-                            src="<?php echo get_template_directory_uri() .'/images/Stilettos &
-Hammers Logo.png' ?>" 
-                            alt="<?php echo esc_html( get_bloginfo( 'name' ) ) ?>" 
-                            class="logo"
-                        >
+                        <hr class="sep-2"> -->
                     </div>
                 </div>
             </footer>
