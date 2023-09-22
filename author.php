8d553382db791cc763b1bac5d663f2430ee83125
Eric Njanga <eric.njanga@gmail.com>
2023-09-21 23:39:33 -0400

    Additional fixes

--- author.php
+++ author.php
@@ -9,21 +9,28 @@
 <?php
     // HERO -----
     // Fetching hero's background image.
-        $hero_background_img = getHeroBgImage();
-    ?>
-    <header class="hero bottom-section-spacer" role="region" style="<?php echo $hero_background_img; ?>;">
-    <div class="text-wrapper">
-        <span class="fs-7">Author page</span>
-        <h1 class="hero-title"><?php echo get_the_author(); ?></h1>
-        <div class="archive-meta" itemprop="description"><?php if ( '' != get_the_author_meta( 'user_description' ) ) { echo esc_html( get_the_author_meta( 'user_description' ) ); } ?></div>
+    $hero_background_img = getHeroBgImage();
+?>
+<header class="hero bottom-section-spacer" role="region" style="<?php echo $hero_background_img; ?>;">
+    <div class="container">
+        <div class="text-wrapper">
+            <span class="fs-7">Author page</span>
+            <h1 class="hero-title"><?php echo get_the_author(); ?></h1>
+            <div style="margin-bottom: 20px;" class="archive-meta" itemprop="description"><?php if ( '' != get_the_author_meta( 'user_description' ) ) { echo esc_html( get_the_author_meta( 'user_description' ) ); } ?></div>
+        </div>
     </div>
 </header>
 
 <main class="main-content-wrapper" role="main">
-    <?php while ( have_posts() ) : the_post(); ?>
-    <?php get_template_part( 'entry' ); ?>
-    <?php endwhile; ?>
-    <?php get_template_part( 'nav', 'below' ); ?>
+
+    <div class="container">
+        <div class="grid">
+            <?php while ( have_posts() ) : the_post(); ?>
+            <?php get_template_part( 'entry' ); ?>
+            <?php endwhile; ?>
+            <?php get_template_part( 'nav', 'below' ); ?>
+        </div>
+    </div>
 </main>
 
 <?php get_footer(); ?>
