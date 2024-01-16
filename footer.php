9d6b06d5eff5bafd100516cbc5fcac8f32b8bb6d
Eric Njanga <eric.njanga@gmail.com>
2024-01-16 08:28:42 -0500

    Correcting markup issue

--- footer.php
+++ footer.php
@@ -3,12 +3,11 @@
     <div class="container footer-container d-flex flex-column">
       <header class="footer-header row">
         <div class="col-lg-6 offset-lg-3 text-center">
-          <a href="" class="navbar-brand"><?php
-		  
-		   $custom_logo_id = get_theme_mod( 'custom_logo' );
-          $logo = wp_get_attachment_image_src( $custom_logo_id , 'full' );
-		 
-		  ?>
+          <a href="" class="navbar-brand">
+            <?php
+              $custom_logo_id = get_theme_mod( 'custom_logo' );
+              $logo = wp_get_attachment_image_src( $custom_logo_id , 'full' );
+            ?>
             <img class="tenverto-logo" src="<?php echo esc_url( $logo[0] ); ?>" alt="Tenverto by Eric Njanga">
           </a>
           <p class="m-0"> <?php echo get_field('sub_heading','option'); ?></p>
@@ -28,71 +27,54 @@
           </ul>
         </div>
       </div>
-	  
-	 
-					</p>
-						 <p>
 					 
-
       <div class="footer-body me-auto ms-auto">
         <div class="footer-body-cell-1">
           <h3 class="footer-body-title">Happiest Clients</h3>
           <ul class="footer-client-list list-no-style m-0 p-0">
-		  
-		  <?php 
-if( have_rows('happiest_clients', 'option') ): 
-    while( have_rows('happiest_clients','option') ) : the_row();
- ?>
-               <li>
-              <a target="_blank" href="<?php echo get_sub_field('linl'); ?>">
-                <img class="client-logo img-fluid" src="<?php echo get_sub_field('image'); ?>" alt="<?php echo get_sub_field('name'); ?>">
-              </a>
-            </li>
-      
-<?php endwhile; endif;?>
-
-           
-		   
+            <?php 
+              if( have_rows('happiest_clients', 'option') ): 
+                while( have_rows('happiest_clients','option') ) : the_row();
+            ?>
+              <li>
+                <a target="_blank" href="<?php echo get_sub_field('linl'); ?>">
+                  <img class="client-logo img-fluid" src="<?php echo get_sub_field('image'); ?>" alt="<?php echo get_sub_field('name'); ?>">
+                </a>
+              </li>
+            <?php endwhile; endif;?>
           </ul>
         </div>
 
         <div>
           <h3 class="footer-body-title">On this website</h3>
           <ul class="footer-body-cell-list-links list-no-style m-0 p-0">
-		  
 		  		  <?php 
-if( have_rows('On this website', 'option') ): 
-    while( have_rows('On this website','option') ) : the_row();
- ?>
-               <li>
-              <a class="link-icon icon-before icon-link" href="<?php echo get_sub_field('linl'); ?>">
-                <?php echo get_sub_field('title'); ?>
-              </a>
-            </li>
-      
-<?php endwhile; endif;?>
-
-             
+              if( have_rows('On this website', 'option') ): 
+                while( have_rows('On this website','option') ) : the_row();
+            ?>
+              <li>
+                <a class="link-icon icon-before icon-link" href="<?php echo get_sub_field('linl'); ?>">
+                  <?php echo get_sub_field('title'); ?>
+                </a>
+              </li>      
+              <?php endwhile; endif;?>
           </ul>
         </div>
 
         <div>
           <h3 class="footer-body-title">Online Presence</h3>
           <ul class="footer-body-cell-list-links list-no-style m-0 p-0">
-		  
-		   <?php 
-if( have_rows('Online Presence', 'option') ): 
-    while( have_rows('Online Presence','option') ) : the_row();
- ?>
-               <li>
-			   <a class="link-icon icon-before<?php echo get_sub_field('additional_class'); ?>" target="_blank"
-               href="<?php echo get_sub_field('linl'); ?>/">
-                <?php echo get_sub_field('title'); ?>
-              </a>
-            </li>
-      
-<?php endwhile; endif;?>
-
+            <?php 
+              if( have_rows('Online Presence', 'option') ): 
+                while( have_rows('Online Presence','option') ) : the_row();
+            ?>
+              <li>
+                <a class="link-icon icon-before<?php echo get_sub_field('additional_class'); ?>" target="_blank"
+                href="<?php echo get_sub_field('linl'); ?>/">
+                  <?php echo get_sub_field('title'); ?>
+                </a>
+              </li>   
+            <?php endwhile; endif;?>
           </ul>
         </div>
       </div>
